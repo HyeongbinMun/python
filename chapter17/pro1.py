abf6734df98b78a30c8f753b544da53fa129f870
HyeongBinMun <39808596+MunHyeongBin@users.noreply.github.com>
2020-01-30 16:22:02 +0900

    cp17.while

---
+++ chapter17/pro1.py
@@ -0,0 +1,4 @@
+money = int(input()) - 1350
+while money >= 0:
+    print(money)
+    money -= 1350
---
+++ chapter17/test.py
@@ -0,0 +1,6 @@
+import random
+
+i = 0
+while i < 7:
+    print(random.randrange(1,7))
+    i += 1

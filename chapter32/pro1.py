28c767d00a8932e03dc5c799805c6683245bce81
HyeongBinMun <39808596+MunHyeongBin@users.noreply.github.com>
2020-04-21 17:42:57 +0900

    cp32. lambda, filter, reduce

--- chapter32/pro1.py
+++
@@ -1,2 +0,0 @@
-files = input().split()
-print(list(map(lambda x: '{0:03d}.{1}'.format(int(x.split('.')[0]), x.split('.')[1]), files)))
--- chapter32/reduce.py
+++
@@ -1,6 +0,0 @@
-from functools import reduce
-def f(x, y):
-    return x+y
-
-a = [1, 2, 3, 4,5]
-print(reduce(f, a))

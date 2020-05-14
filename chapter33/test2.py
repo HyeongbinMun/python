84b107dda5c4d21edc213d99414b36c882ad53df
HyeongBinMun <39808596+MunHyeongBin@users.noreply.github.com>
2020-05-14 14:35:43 +0900

    주석 추가

--- chapter33/test2.py
+++ chapter33/test2.py
@@ -1,11 +1,12 @@
 def A():
-    x = 10
-    y = 100
+    x = 10              # function A 의 지역 변수
+    y = 100             # function A 의 지역 변수
     def B():
-        x = 20
+        x = 20          # function B 의 지역 변수
         def C():
-            nonlocal x
-            nonlocal y
+            """nonlocal : 상위의 지역 혹은 전역 변수 호출(계단형 검색)"""
+            nonlocal x  # function A 의 지역 변수 호출
+            nonlocal y  # function B 의 지역 변수 호출
             x = x + 30
             y = y + 300
             print(x)
